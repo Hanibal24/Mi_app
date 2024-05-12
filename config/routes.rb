51d1fbd091c55831151dfa42f5608867e674ff5a
Hanibal24 <anibal.villegas@uc.cl>
2024-05-11 22:34:05 -0400

    render

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
-  root to: 'render#index'
+  root to: 'home#index'
   devise_scope :user do
     get 'users/sign_out' => "devise/sessions#destroy"
   end
